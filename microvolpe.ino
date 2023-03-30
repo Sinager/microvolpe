9239676d553c2b54e2a49be74cf3823bd1a5e666
Sinager <sinageruk@gmail.com>
2023-03-30 20:36:40 +0200

    Update microvolpe.ino
    modifiche suggerite da Achille IU0EUF

--- microvolpe.ino
+++ microvolpe.ino
@@ -1,49 +1,49 @@
 /*
-codice per microvolpe
-IZ0MJE 2023 - GNU/GPL
+ * ----------------------------------------------------------------
+ * codice per microvolpe
+ * IZ0MJE 2023 - GNU/GPL
+ * modificato da IU0EUF
+ * ----------------------------------------------------------------
 */
 
-const int frqTono = 700;
-const int pinTx = 2; // pin a cui collegare tx del modulo RF
+// --------- I/O -----------------------------------------------------
+// #define pinTx   2           // pin a cui collegare TX del modulo RF
+#define pinTx   12           // pin a cui collegare TX del modulo RF
+// -------------------------------------------------------------------
 
-void setup() {
+const int frqTono=700;
+const int T1=200;
+const int T3=600;
+const int TL=2000;
+const int BE=1000;
+
+void setup() 
+{
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(pinTx, OUTPUT);
 }
 
-void linea(){
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+void manovra(int t)
+{
+  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED on (HIGH is the voltage level)
   tone(pinTx,frqTono);
-  delay(600);                       // wait
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(t);                           // wait
+  digitalWrite(LED_BUILTIN, LOW);     // turn the LED off by making the voltage LOW
   noTone(pinTx);
-  delay(200);                       // wait
-  }
+  delay(T1);                          // wait
+}
 
-void punto(){
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  tone(pinTx,frqTono);
-  delay(200);                       // wait
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  noTone(pinTx);
-  delay(200);                       // wait
-  }
+void punto()      {  manovra(T1);  }
+void linea()      {  manovra(T3);  }
+void tonoLungo()  {  manovra(TL); }
 
-void tonoLungo(){
-  delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  tone(pinTx,frqTono);
-  delay(2000);                       // wait
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  noTone(pinTx);
-  delay(200);                       // wait
-  }
-
-// the loop function runs over and over again forever
-void loop() {
+                    
+void loop()               // the loop function runs over and over again forever
+{
   linea();
   linea();
   linea();
+  delay(TL);
   tonoLungo();
-  delay(1000);
+  delay(BE);
 }
